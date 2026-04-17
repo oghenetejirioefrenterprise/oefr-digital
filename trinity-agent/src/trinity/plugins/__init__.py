cf4121a499fdeaa7ddb5cc449c2c27d9c90d70bf
Trinity <trinity@oefrenterprise.com>
2026-04-17 14:55:25 -0400

    feat(plugins): add ProviderSpec, ToolSpec, AgentSpec dataclasses

--- trinity-agent/src/trinity/plugins/__init__.py
+++ trinity-agent/src/trinity/plugins/__init__.py
@@ -3,5 +3,14 @@ from __future__ import annotations
 
 from .discovery import scan
 from .registry import AlreadyRegistered, NotRegistered, Registry
+from .specs import AgentSpec, ProviderSpec, ToolSpec
 
-__all__ = ["Registry", "AlreadyRegistered", "NotRegistered", "scan"]
+__all__ = [
+    "Registry",
+    "AlreadyRegistered",
+    "NotRegistered",
+    "scan",
+    "AgentSpec",
+    "ProviderSpec",
+    "ToolSpec",
+]
